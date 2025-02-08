456eb6946d9e98844f021fe51ae25d1e7bffc646
Marcin Fudala <nerko937@gmail.com>
2025-02-08 12:20:51 +0100

    Removed fullscreen

--- main.lua
+++ main.lua
@@ -8,9 +8,8 @@ local CANVAS_WIDTH = 720
 local CANVAS_HEIGHT = 1280
 
 function love.load()
-	love.window.setFullscreen(true)
-	Piles.recreate()
     love.graphics.scale(1/love.window.getDPIScale())
+	Piles.recreate()
     mx = 0
     my = 0
     tx = 0
